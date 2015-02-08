ba5c540599984a846ce1028ce115a9751ebcbf57
Pedro Morales Rodriguez <pedromoralesrodriguez@hotmail.com>
2015-02-08 21:56:35 +0000

    Uploaded main files
    All the main files for the template

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+
+		<!-- SCRIPTS -->
+	<script src="/scripts/main.js" type="text/javascript" charset="utf-8" async defer></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+		<!-- META -->
+	<title></title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+
+		<!-- CSS LINKS -->
+	<link rel="stylesheet" href="./css/reset.css">	
+			<!-- GRID -->
+	<link rel="stylesheet" media="screen and (min-width: 720px)" href="./css/720_grid.css">
+	<link rel="stylesheet" media="screen and (min-width: 986px)" href="./css/986_grid.css">
+	<link rel="stylesheet" media="screen and (min-width: 1236px)" href="./css/1236_grid.css">
+			<!-- GRID ENDS -->
+	<link rel="stylesheet" href="./css/style.css">
+
+		<!-- FONTS -->
+</head>
+<body>
+
