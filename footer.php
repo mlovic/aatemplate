488152725de8a7e11f7037ab6ba2d4a641349fa1
Pedro Morales Rodriguez <pedromoralesrodriguez@hotmail.com>
2015-02-09 01:51:46 +0000

    Changed the color of the h2, it looked bad.

--- footer.php
+++ footer.php
@@ -35,6 +35,6 @@
 
 
 		<!-- SCRIPTS -->
-	<script src="/scripts/main.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="./scripts/main.js" type="text/javascript" charset="utf-8" async defer></script>
 </body>
 </html>
